028be0f4fb1409d418d915f34bd87bca716023a0
alexandre96dev <alexandre.nascimento96dev@gmail.com>
2024-07-18 03:43:45 -0300

    teste7

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('hello')
+  getTeste(@Res() response: Response) { 
+    response.status(200).json({
+      message: 'hellooooooo'
+    })
   }
 }
